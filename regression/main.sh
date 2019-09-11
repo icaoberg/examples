501f0ba07faf44cd7ed14052c7d77bb36e747e03
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2019-09-11 14:45:34 -0400

    Added script

---
+++ regression/main.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+#$ -j y
+#$ -S /bin/bash
+#$ -cwd
+
+## the next line selects the partition/queue
+#SBATCH -p model3
+
+## the next line selects the number of nodes
+#SBATCH -N 1
+
+## the next line selects the number of CPUs
+#SBATCH -n 1
+
+## the next line selects the memory size
+#SBATCH --mem=8G
+
+## the next line selects the walltime
+#SBATCH -t 24:00
+
+## select GPU
+#SBATCH --gres=gpu:1
+
+cat << EOF > script.sh
+#!/bin/bash
+
+python main.py
+EOF
+
+singularity exec --nv /containers/images/u1604-cuda-9.1_pytorch.img bash ./script.sh
+
